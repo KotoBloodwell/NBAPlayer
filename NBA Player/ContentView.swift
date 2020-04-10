4df23f1182855a5b64925f6bad6ba78f8552b9e4
Juliana Loaiza <julianaloaiza27@gmail.com>
2020-04-10 14:57:23 -0500

    Initial Commit

---
+++ NBA Player/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA Player
+//
+//  Created by Juliana Loaiza Labrador on 10/04/20.
+//  Copyright © 2020 Juliana Loaiza Labrador. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
